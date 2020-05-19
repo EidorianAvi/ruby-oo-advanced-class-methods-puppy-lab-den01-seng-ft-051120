b12992110a6fd16634b88939be8814e3040b839e
Rene Avila <reneavila1993@gmail.com>
2020-05-19 21:44:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+# Add your code herec
+
+class Dog 
+  @@all = []
+  
+  attr_reader :name
+  
+  def initialize name 
+    @name = name 
+    
+    self.save
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    all.each{|dog| puts dog.name}
+  end
+  
+  def self.clear_all
+    all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+
+  
+
+
+
+
+end
